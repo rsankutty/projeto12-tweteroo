abdf50732f105fa221159e79059fd905c4c06fa0
rsankutty <r.sankutty@gmail.com>
2023-01-08 17:05:29 -0300

    rotas post signup e tweets

--- src/app.js
+++ src/app.js
@@ -1,16 +1,48 @@
 import express from "express";
 import cors from 'cors';
 
+//porta da aplicação
+const PORT = 5000;
+
+//inicializando servidor
 const server = express();
 server.use(cors());
-
-//definindo porta do servidor e printando msg para o server rodando
-server.listen(5000,() => {
-    console.log("Servidor Rodando")
+server.use(express.json());
+server.listen(PORT,() => {
+    console.log(`Servidor rodando na porta ${PORT}`)
 });
 
-//definindo rota /
-server.get("/", (req, res) => {
-    // Manda como resposta o texto 'Hello World'
-    res.send('Hello World')
+
+const usuarios = []
+const tweets = []
+
+//definindo rota post de sign-up
+server.post('/sign-up', (req, res) => {
+  const usuario = req.body;
+  console.log(usuario);
+  usuarios.push(usuario);
+  res.send("OK")
 });
+
+//definindo rota post de tweets
+server.post('/tweets', (req, res) => {
+    const tweet = req.body;
+    console.log(tweet);
+
+    const username = tweet.username
+
+    const usuarioCadastrado = usuarios.find(item => item.username === username)
+    if(!usuarioCadastrado){
+        res.send("UNAUTHORIZED")
+    } else {
+        tweets.push({...tweet, avatar: usuarioCadastrado.avatar})
+        res.send("OK")
+    }
+  });
+
+
+//definindo rota get de tweets
+// server.get('/tweets', (req, res) => {
+//    let tweetsArr=[]
+   
+// });
